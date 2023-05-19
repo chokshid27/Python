7b23d2612078b209ab04564fa8171a5a8beadae3
chokshid27 <126411094+chokshid27@users.noreply.github.com>
2023-05-19 22:53:26 +0530

    Add files via upload

---
+++ Simple Statistics with a List of Numbers.py
@@ -0,0 +1,9 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> digits=[1,2,3,4,5,6,7,8,9,0]
+>>> min(digits)
+0
+>>> max(digits)
+9
+>>> sum(digits)
+45
