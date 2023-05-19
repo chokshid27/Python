af514438354a2328c59bf079bdf231b7ba92559e
chokshid27 <126411094+chokshid27@users.noreply.github.com>
2023-05-19 21:31:38 +0530

    Add files via upload

---
+++ The Zen of Python.py
@@ -0,0 +1,8 @@
+>>> import this
+The Zen of python, by Tin Peters
+Beautiful is better than ugly.
+Simple is better than complex.
+Complex is better than complicated.
+Readability counts.
+There should be one-- and preferably only one--obvious way to do it.
+Now is better than never.
